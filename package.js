c4659029a4b351fc573aa4d001d799744322d9be
dreamxyp <dreamxyp@gmail.com>
2018-05-02 12:34:45 +0800

    package
    package

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 /* jshint ignore:start */
 Package.describe({
-  name: 'silentcicero:happyucjs-accounts',
+  name: 'happyucjs:happyucjs-accounts',
   version: '0.0.10',
   summary: 'A simple module for creating, managing and using HappyUC accounts in browser',
-  git: 'https://github.com/SilentCicero/happyucjs-accounts',
+  git: 'https://github.com/happyucjs/happyucjs-accounts',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,7 +12,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
 
-  api.export(['Accounts'], ['client']);
+  api.export(['accounts'], ['client']);
 
   api.addFiles('dist/happyucjs-accounts.js', 'client');
   api.addFiles('package-init.js', 'client');
